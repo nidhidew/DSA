88b8680d33c7768b8246214c591e883f00356868
nidhidew <nidhicec97@gmail.com>
2024-12-04 12:20:17 +0530

    conditional statements

--- 1.1 language_basics/Conditions and loops/conditional_statements.js
+++ 1.1 language_basics/Conditions and loops/conditional_statements.js
@@ -1,34 +1,76 @@
-// if
-if(condition){
-//expression
-}
+const readline = require("readline");
 
-//if else
-if(condition){
-//expression
-}else{
-//expression
-}
+const r1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
 
-//nested if
-if(condition){
-//expression
-}else if(condition){
-//expression
-}else{
-//expression
-}
+/*
+r1.question("1st number:",(num1) => {
+    r1.question("2nd number:",(num2) => {
+        //if condition
+        if(num1 < num2){
+            console.log(num1," is smaller");
+        }
+        r1.close();
+    })
+})
+*/
+/*
+r1.question("1st number:",(num1) => {
+    r1.question("2nd number:",(num2) => {
+        //if else condition
+        if(num1 < num2){
+            console.log(num1," is smaller");
+        } else {
+            console.log(num2," is smaller")
+        }
+        r1.close();
+    })
+})
+*/
+/*
+r1.question("1st number:",(num1) => {
+    r1.question("2nd number:",(num2) => {
+        //if else nested condition
+        if(num1 < num2){
+            console.log(num1," is smaller");
+        } else if(num1 > num2){
+            console.log(num2," is smaller")
+        } else if(num1 === num2){
+            console.log("both input is same")
+        } else {
+            console.log("invalid input");
+        }
+        r1.close();
+    })
+})
+*/
+/*
+r1.question("1st number:",(num1) => {
+    r1.question("2nd number:",(num2) => {
+        //ternary operator
+        num1 < num2 ? console.log(num2+" is bigger") : console.log(num1+" is bigger");
+        r1.close();
+    })
+})
+*/
+/*
+r1.question("1st number:",(num1) => {
+    r1.question("2nd number:",(num2) => {
+        //switch case
 
-//ternary operator
-condition ? expressionIfTrue : expressionIfFalse;
-
-//swicth
-switch(expression){
-    case value1:
-        //expression
-        break;
-    case value2:
-        //expression
-        break;
-    default:
-}
+        switch(true){
+            case num1 > num2:
+                console.log(num1+" is bigger")
+                break;
+            case num2 > num1:
+                console.log(num2+" is bigger")
+                break;
+            default:
+                console.log("invalid inputs");
+        }
+        r1.close();
+    })
+})
+*/
