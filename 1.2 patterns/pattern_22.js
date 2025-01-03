3b4631aa2282fc8b054f6c5fea3af6a6f9b0a77e
nidhidew <nidhicec97@gmail.com>
2025-01-03 17:39:40 +0530

    pattern 22 is incomplete

--- 1.2 patterns/pattern_22.js
+++ 1.2 patterns/pattern_22.js
@@ -1,33 +1,33 @@
-/*
+// /*
 
-4 4 4 4 4 4 4
-4 3 3 3 3 3 4
-4 3 2 2 2 3 4
-4 3 2 1 2 3 4
-4 3 2 2 2 3 4
-4 3 3 3 3 3 4
-4 4 4 4 4 4 4
+// 4 4 4 4 4 4 4
+// 4 3 3 3 3 3 4
+// 4 3 2 2 2 3 4
+// 4 3 2 1 2 3 4
+// 4 3 2 2 2 3 4
+// 4 3 3 3 3 3 4
+// 4 4 4 4 4 4 4
 
-*/
+// */
 
-const readline = require("readline");
-const r1 = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+// const readline = require("readline");
+// const r1 = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+// });
 
-r1.question("number: ", (n) => {
-    for(let i=1; i<=n*2-1; i++){
-        let pattern = "";
-        for(let j=1; j<=n*2-1; j++){
-            let star;
-            // pattern += j+" ";
-            pattern += n-i;
-            if(i==1 || j==1 || i==n-1 || j==n-1){
-                // pattern += n-i;
-            }
-        }
-        console.log(pattern);
-    }
-    r1.close();
-});
+// r1.question("number: ", (n) => {
+//     for(let i=1; i<=n*2-1; i++){
+//         let pattern = "";
+//         for(let j=1; j<=n*2-1; j++){
+//             let star;
+//             // pattern += j+" ";
+//             pattern += n-i;
+//             if(i==1 || j==1 || i==n-1 || j==n-1){
+//                 // pattern += n-i;
+//             }
+//         }
+//         console.log(pattern);
+//     }
+//     r1.close();
+// });
