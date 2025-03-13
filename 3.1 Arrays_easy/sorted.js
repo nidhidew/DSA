c3368c1576577594593fbd259e7fcd6d81dca353
Nidhi Dewangan <41151235+nidhidew@users.noreply.github.com>
2025-03-13 12:24:34 +0530

    sorting problem is solved

--- 3.1 Arrays_easy/sorted.js
+++ 3.1 Arrays_easy/sorted.js
@@ -1,41 +1,29 @@
-let arr = [5,4,6,7,8];
-console.log(arr )
-console.log(arr.sort())
-// console.log(arr === arr.sort())
-// if(arr === arr.sort()){
-//     console.log(true)
-// }else{
-//     console.log(false)
-// }
+// let nums = [2, 1, 3, 4];
+// let nums = [3,4,5,1,2];
+let nums = [3,6,10,1,8,9,9,8,9];
+let countDrop = 0;
+let n = nums.length;
 
-/*
-var check = function(nums) {
-    let newNums = [...nums].sort();
-    let numsEle = nums[0];
-
-function rotate(arr,x){
-    for(let j=0;j<x;j++){
-        arr.push(arr.shift());
+for (let i = 0; i < n; i++) {
+    // Check if the next element is smaller than the current element
+    if (nums[i] > nums[(i + 1) % n]) {
+        countDrop++;
+    }
+    
+    // If there is more than one drop, it's not a rotated sorted array
+    if (countDrop > 1) {
+        return false;
     }
 }
-    if(nums.length == 1){
-        return true;
-    }else if(nums.length >= 2){
-        for(let i=1;i<nums.length;i++){
-            if(numsEle <= nums[i]){
-                numsEle = nums[i];
-                return true;
-            }
-            else if(numsEle > nums[i]){
-                rotate(nums,i)
-                numsEle = nums;
-                if(newNums.every((val, index) => val === numsEle[index])){
-                    return true;
-                }else if(newNums.every((val, index) => val !== numsEle[index])){
-                    return false;
-                }
-            }
-        }
-    }
-};
-*/
+
+return true;
+
+// How It Works:
+
+//     Loop through the array.
+//     If nums[i] > nums[(i + 1) % n] → It’s a drop.
+//     If the number of drops exceeds 1 → Return false.
+//     If the loop finishes with zero or one drop → Return true.
+
+/* If an array is rotated, there will be exactly one drop where the largest element meets the smallest element.If it’s completely sorted (no rotation), 
+ there will be zero drops.*/
