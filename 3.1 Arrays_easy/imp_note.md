778a3428972f36622f57dae818f59cad64d437dc
Nidhi Dewangan <41151235+nidhidew@users.noreply.github.com>
2025-03-15 12:11:46 +0530

    rotate array problem is solved

--- 3.1 Arrays_easy/imp_note.md
+++ 3.1 Arrays_easy/imp_note.md
@@ -1,3 +1,13 @@
 # IMPORTANT NOTE
 
 * if need to find largest number, then assign a dummy value to the variable to store the largest number in that variable during for loop and conditions.
+
+* Why Cyclic Dependency?
+
+    When you rotate an array, each element’s new position is calculated using:
+
+        newIndex=(i+k)%n
+        newIndex=(i+k)%n
+
+    This creates a cyclic shift — elements are essentially "wrapping around" the array boundaries.
+    The cyclic behavior comes from the % n part — that's why this is a modulo-based rotation problem. 
--- 3.1 Arrays_easy/rotate_arr.js
+++ 3.1 Arrays_easy/rotate_arr.js
@@ -1,24 +1,12 @@
 let nums = [1, 2, 3, 4, 5, 6, 7];
 let k = 3;
-let rotate = new Array(nums.length);
+let rotateArr = new Array(nums.length);
 
 for(let i=0;i<nums.length;i++){
     const newIndex = (i+k) % (nums.length);
-    rotate[newIndex] = nums[i];
+    rotateArr[newIndex] = nums[i];
 }
-console.log(rotate);
-
-/**
- let arr = [1, 2, 3, 4, 5];
-let k = 2;
-let n = arr.length;
-
-let rotated = new Array(n);
-
-for (let i = 0; i < n; i++) {
-    let newIndex = (i + k) % n;
-    rotated[newIndex] = arr[i];
+for(let j=0;j<nums.length;j++){
+    nums[j] = rotateArr[j];
 }
-
-console.log(rotated); // Output: [4, 5, 1, 2, 3]
- */
+console.log(nums);
