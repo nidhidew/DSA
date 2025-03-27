dabcdacb75c4229e4881f6dac38ec7e75a3e143d
Nidhi Dewangan <41151235+nidhidew@users.noreply.github.com>
2025-03-28 00:42:19 +0530

    explaination is done in loop_for_array

--- 3.1 Arrays_easy/loop_for_array.js
+++ 3.1 Arrays_easy/loop_for_array.js
@@ -3,5 +3,17 @@
 let arr = [1,2,3,4,5]
 
 for(let i=0;i<arr.length;i++){
-    console.log(arr[i])
-} //for 5 time itrations
+    // console.log(arr[i]) //1,2,3,4,5
+} //for 5 time itrations from 0th index to 4th index
+
+for(let i=1;i<arr.length;i++){
+    // console.log(arr[i]) //2,3,4,5
+} //for 4 time iteration from 1st index to 4th index
+
+for(let i=0;i<arr.length-1;i++){
+    // console.log(arr[i]); //1,2,3,4
+} //for 4 time iteration from 0th index to 3rd index
+
+for(let i=1;i<arr.length-1;i++){
+    // console.log(arr[i]) //2,3,4
+} //for 3 time iteration from 1st index to 3rd index
