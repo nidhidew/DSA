1f525ddfa11351c24f63608862eb808bff952023
nidhidew <nidhicec97@gmail.com>
2025-04-11 21:28:05 +0530

    updated code for brute force approach to remove duplicates from sorted array

--- 3.1 Arrays_easy/rmv_duplicates.js
+++ 3.1 Arrays_easy/rmv_duplicates.js
@@ -13,3 +13,29 @@ for(let i=0;i<nums.length;i++){
 }
 k=j;
 console.log(k,nums);
+
+//Brute force
+let arr = [0,0,1,1,1,2,2,3,3,4];
+let pointer = Infinity;
+let swap = null;
+let pos = 0;
+
+for (let i = 0; i < arr.length; i++) {
+  if (pointer === arr[i]) {
+    pointer = arr[i];
+    arr[i] = null;
+  } else {
+    pointer = arr[i];
+  }
+  if (arr[i] != null) {
+    swap = arr[i];
+    arr[i] = arr[pos];
+    arr[pos] = swap;
+    pos++;
+  }
+}
+while (arr[arr.length - 1] == null) {
+  --arr.length;
+}
+
+console.log(arr);
