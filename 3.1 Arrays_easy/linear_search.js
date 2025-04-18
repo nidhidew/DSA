49fffa2d6398dfd78ca32dd4f718dd752d86d6d1
nidhidew <nidhicec97@gmail.com>
2025-04-18 20:00:21 +0530

    another type of question,algorithm for linear search

--- 3.1 Arrays_easy/linear_search.js
+++ 3.1 Arrays_easy/linear_search.js
@@ -12,3 +12,15 @@ console.log(index);
 
 // Given an array of integers nums and an integer target, find the smallest index (0 based indexing) where the target appears in the array. 
 // If the target is not found in the array, return -1
+let arr2 = [5,51,4,7,8]
+let target2 = 51
+let index2 = null;
+for(let i=0;i<arr2.length;i++){
+  if(arr2[i] == target2){
+    index2 = i;
+    i = arr2.length
+  }else{
+    index2 = -1;
+  }
+}
+console.log(index);
