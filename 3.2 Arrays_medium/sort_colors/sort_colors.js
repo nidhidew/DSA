73f9f2d19538e3ceef94b0da40d32d9c709ad342
nidhidew <nidhicec97@gmail.com>
2025-05-12 17:05:06 +0530

    added 2 medium level question algorithms

---
+++ 3.2 Arrays_medium/sort_colors/sort_colors.js
@@ -0,0 +1,16 @@
+let arr = [2,0,2,1,1,0];
+let sorted = false;
+
+while(!sorted){
+    sorted = true;
+    for(let i=0;i<arr.length-1;i++){
+        if(arr[i] > arr[i+1]){
+            let swap = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = swap;
+            sorted = false;
+        }
+    }
+}
+
+console.log(arr)
