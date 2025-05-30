6f658b2af967b3f06249090beed5dd2945018f25
nidhidew <nidhicec97@gmail.com>
2025-05-30 12:17:17 +0530

    algorithm pattern added for sliding window

--- logic_solving_patterns/sliding_window/sliding_window.js
+++ logic_solving_patterns/sliding_window/sliding_window.js
@@ -1,18 +1,44 @@
+// (1) fixed window size(e.g. subarray of size k)
+/** when in a question tells/asked about:
+ * Find the maximum sum of any subarray of size k
+ * Find the average of every window of size k
+ * Count of items in each group of k elements
+ * Analyze fixed-length time intervals, subarrays, or chunks
+ */
+
 let arr = [2,1,5,1,3,2];
 let k = 3;
 let maxSum = 0;
 let windowSum = 0;
-let start = 0;
 for (let i = 0; i < k; i++) {
   windowSum = windowSum + arr[i];
 }
 maxSum = windowSum;
-
 //slide the window forward
-for (let end = k; end < arr.length; end++) {
-  windowSum = windowSum + arr[end] - arr[start]; // slide the window
-  start++;                            // move start forward
+for (let j = k; j < arr.length; j++) {
+  windowSum = windowSum + arr[j] - arr[j-k]; // slide the window
   maxSum = Math.max(maxSum, windowSum); // update max if needed
 }
-
 console.log(maxSum)
+
+// (2) dynamic window size(e.g. maximum subarray)
+/** when in a question tells/asked about:
+ * Have sum ≤ or ≥ k
+ * Contain unique elements
+ * Contain at most k distinct elements
+ * Match a pattern or all target characters
+*/
+
+/*ALGORITHM */
+
+// let i=0; //start point of window
+// let windowsum = 0;
+// for(let j=0;j<arr.length;j++){ //j is end point of window
+//   windowsum = windowsum + arr[j];
+//   while(/*window does not satisfy the condition*/){
+//     //shrink the window from the left
+//     //update the state
+//     i++;
+//   }
+//   let windowsum = j-i+1;
+// }
